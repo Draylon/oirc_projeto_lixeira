b49ddfb3619644b51ef0aa5c046321314806a882
Draylon Vieira <draylid@gmail.com>
2022-06-09 00:04:33 -0300

    1st commit 🎉

---
+++ LixeiraMQ_Server/LixeiraMQ_Server.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LixeiraMQ_Server.ino
+ Created:	08/06/2022 11:59:46 PM
+ Author:	drayl
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
